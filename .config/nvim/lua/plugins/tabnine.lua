7e4ba764064194810b26768fa6a43363eeaaa7e1
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-10-03 05:50:57 +0100

    comment out the tabnine configuration

--- .config/nvim/lua/plugins/tabnine.lua
+++ .config/nvim/lua/plugins/tabnine.lua
@@ -1,26 +1,18 @@
--- Get platform dependant build script
-local function tabnine_build_path()
-	-- Replace vim.uv with vim.loop if using NVIM 0.9.0 or below
-	if vim.uv.os_uname().sysname == "Windows_NT" then
-		return "pwsh.exe -file .\\dl_binaries.ps1"
-	else
-		return "./dl_binaries.sh"
-	end
-end
+local method = require("config.method")
 
 return {
-	"codota/tabnine-nvim",
-	build = tabnine_build_path(),
-	config = function()
-		require("tabnine").setup({
-			disable_auto_comment = true,
-			accept_keymap = "<Tab>",
-			dismiss_keymap = "<C-]>",
-			debounce_ms = 800,
-			suggestion_color = { gui = "#808080", cterm = 244 },
-			exclude_filetypes = { "TelescopePrompt", "NvimTree" },
-			log_file_path = nil, -- absolute path to Tabnine log file
-			ignore_certificate_errors = false,
-		})
-	end,
+	-- "codota/tabnine-nvim",
+	-- build = method.is_win() and "pwsh.exe -file .\\dl_binaries.ps1" or "./dl_binaries.sh",
+	-- config = function()
+	-- 	require("tabnine").setup({
+	-- 		disable_auto_comment = true,
+	-- 		accept_keymap = "<Tab>",
+	-- 		dismiss_keymap = "<C-]>",
+	-- 		debounce_ms = 800,
+	-- 		suggestion_color = { gui = "#808080", cterm = 244 },
+	-- 		exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+	-- 		log_file_path = nil, -- absolute path to Tabnine log file
+	-- 		ignore_certificate_errors = false,
+	-- 	})
+	-- end,
 }
